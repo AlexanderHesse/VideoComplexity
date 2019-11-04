29f8feef6591f736b304f4a5ad16d256edf659ad
Alexander Hesse <me@alexander-hesse.com>
2019-11-04 13:46:57 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+## About
+
+This is a tool to measure the complexity of an image or video by various methods. Currently, the following methods are available:
+
+Method Name | Description
+--- | ---
+image:box | Blurs an image using a box filter and returns the absolute difference
+image:iterated_box | Applies image:box repeatedly, each time quartering the resolution and quartering the weight of the result
+image:gaussian | Blurs an image using a gaussian filter and returns the absolute difference
+image:iterated_gaussian | Applies image:gaussian repeatedly, each time quartering the resolution and quartering the weight of the result
+image:fft | Reduces the frequencies of the image linearily and returns the sum of all pixels
+image:webp | Returns the file size of the image converted to the webp format
+video:box | Like image:box, but applies the kernel to the time axis as well
+video:gaussian | Like image:gaussian, but applies the kernel to the time axis as well
+
+The results are written in CSV format on stdout.
+
+Since each method has its own scale of complexity, a rescale tool is provided to normalize the results. Currently, the following methods are available:
+
+* scale by the geometric average of a reference result file (-g)
+* scale by normalizing to the first frame of each video (-b)
+
+## Requirements
+
+* Python 2
+* Numpy
+* OpenCV
+* Scipy
+
+## Usage
+
+```
+video_complexity.py [-v] [-m/--methods <methods>] [-o <out file>] <media>+
+rescale.py [-v] [-g <reference file> | -b <base value>] [-o <out file>] <csv file>
+```
+
+Methods are specified comma-separated and without spaces.
+
+## Examples
+
+```
+$ video_complexity.py -v reference_videos/*.mp4 -o reference.csv
+$ video_complexity.py -v ducks.mp4 -o ducks.csv
+$ rescale.py -g reference.csv ducks.csv -o ducks_normalized.csv
+```
+
+```
+$ video_complexity.py -m image:gaussian,image:webp,video:gaussian -o ducks.csv ducks.mp4
+$ rescale.py -b 1000 -o ducks_normalized.csv ducks.csv
+```
+
+## Implementation Details
+
+* Image methods expect a single image argument, returning the measured complexity.
+* Video methods expect a single generator argument. The generator yields a tuple of the frame index and the frame itself. The video method is a generator itself, yielding a tuple of the frame index and the measured complexity.
+* Each image or frame is given as a numpy array of floats in the range [0-1].
+
+For testing new methods, the following images are provided:
+
+Image | Description | Expected result
+--- | --- | ---
+00_a.png | a reference circle | reference value
+01_a_space.png | the same reference circle in a larger image, with more space around it | approximately the reference value
+02_2a.png | two of the reference circle | approximately twice the reference value
+03_a_2x.png | the reference circle scaled 2x in each dimension | approximately the reference value
+04_a_4x.png | the reference circle scaled 4x in each dimension | approximately the reference value
---
+++ rescale.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- encoding: utf8 -*-
+
+from __future__ import division
+
+from math import exp, log
+import sys
+
+from video_complexity import read_csv, write_csv
+
+
+def group_by(rows, field):
+	groups = {}
+	for row in rows:
+		groups.setdefault(row.get(field), list()).append(row)
+	for i in sorted(groups.keys()):
+		yield groups[i]
+
+def columns(rows):
+	if len(rows) > 0:
+		for c in range(len(rows[0])):
+			yield rows[0].columns[c], [row[c] for row in rows]
+
+
+def scaled(data, factors, verbose):
+	if verbose:
+		for method, factor in factors.iteritems():
+			print >> sys.stderr, method, factor
+	
+	for row in data:
+		yield row.map(lambda col, val: val * factors[col] if col in factors and val != None else val)
+
+
+def geometric_mean(l):
+	return exp(sum(log(i) for i in l)/len(l))
+
+def by_geometric_mean(reference_data, apply_data, verbose = False):
+	methods = [i for i in reference_data[0].columns if i not in ('path', 'frame_index')]
+	data = [row for row in reference_data if None not in row]
+		
+	by_video = group_by(data, 'path')
+	avg_by_video = [[geometric_mean(l) for col, l in columns(rows) if col in methods] for rows in by_video]
+	avg_all = [geometric_mean(l) for l in zip(*avg_by_video)]
+	avg_all = [i/geometric_mean(avg_all) for i in avg_all]
+	
+	factors = { m:1/i for m, i in zip(methods, avg_all) }
+	
+	for row in scaled(apply_data, factors, verbose):
+		yield row
+
+
+def by_first_frame(base, data, verbose = False):
+	methods = [i for i in data[0].columns if i not in ('path', 'frame_index')]
+	
+	by_path = group_by(data, 'path')
+	for rows in by_path:
+		if rows[0].get('frame_index') == None:
+			# it's an image
+			for row in rows:
+				yield row
+			continue
+			
+		rows = sorted(rows, key = lambda row: row.get('frame_index'))
+		first = [row for row in rows if None not in row][0]
+		
+		factors = { m:base/first.get(m) for m in methods }
+		
+		for row in scaled(rows, factors, verbose):
+			yield row
+	
+
+if __name__ == '__main__':
+	import argparse
+	
+	parser = argparse.ArgumentParser()
+	parser.add_argument("csv", help="the csv file to rescale")
+	parser.add_argument("-g", metavar="REFERENCE_CSV_PATH", help="scale by the geometric average of a reference result csv file")
+	parser.add_argument("-b", metavar="BASE_NUM", type=float, help="scale by normalizing to the first frame of each video")
+	parser.add_argument("-v", "--verbose", help="verbose", action="store_true")
+	parser.add_argument("-o", "--output", help="write output to file")
+	
+	args = parser.parse_args()
+	
+	data = list(read_csv(open(args.csv)))
+	
+	gen = None
+	if args.b != None:
+		gen = by_first_frame(args.b, data, args.verbose)
+	elif args.g != None:
+		reference_data = list(read_csv(open(args.g)))
+		gen = by_geometric_mean(reference_data, data, args.verbose)
+	
+	if gen == None:
+		sys.exit(0)
+	
+	if args.output != None:
+		with open(args.output, 'w') as f:
+			write_csv(gen, f)
+	else:
+		write_csv(gen, sys.stdout)
